1efbcf09eba13628a5826125bfcc111ee4d507e3
pi9min <pi9min@users.noreply.github.com>
2016-05-20 00:11:57 +0900

    tmp2

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -2,7 +2,19 @@ class Api::V1::VideosController < ApplicationController
   protect_from_forgery with: :null_session
 
   def all
-    videos = Video.all.order(recorded_at: :desc)
-    render json: videos
+    json = { googlevideos: [] }
+    videos_recently = Video.all.published.order(recorded_at: :desc).limit(100)
+    json[:googlevideos] << {
+      category: '最近録画された動画',
+      videos: videos_recently.map { |v| { title: v.title, description: v.description, studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
+    }
+    Video.categories.each do |category_name, category_number|
+      next if category_name == 'unknown'
+      json[:googlevideos] << {
+        category: t("enums.video.category.#{category_name}"),
+        videos: Video.all.published.where(category: category_number).order(recorded_at: :desc).limit(100).map { |v| { title: v.title, description: v.description, studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
+      }
+    end
+    render json: json
   end
 end
