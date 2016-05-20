efdd85fb95ea4e7d6bfc4f77653733303b1fbf11
pi9min <pi9min@users.noreply.github.com>
2016-05-21 00:20:12 +0900

    tmp3

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -6,15 +6,22 @@ class Api::V1::VideosController < ApplicationController
     videos_recently = Video.all.published.order(recorded_at: :desc).limit(100)
     json[:googlevideos] << {
       category: '最近録画された動画',
-      videos: videos_recently.map { |v| { title: v.title, description: v.description, studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
+      videos: videos_recently.map { |v| { title: v.title, description: self.decorate_description(v.chapter, v.description), studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
     }
     Video.categories.each do |category_name, category_number|
       next if category_name == 'unknown'
       json[:googlevideos] << {
         category: t("enums.video.category.#{category_name}"),
-        videos: Video.all.published.where(category: category_number).order(recorded_at: :desc).limit(100).map { |v| { title: v.title, description: v.description, studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
+        videos: Video.all.published.where(category: category_number).order(recorded_at: :desc).limit(100).map { |v| { title: v.title, description: self.decorate_description(v.chapter, v.description), studio: 'Ponpe', card: v.thumbnail_url, background: v.thumbnail_url, sources: [v.url] }}
       }
     end
     render json: json
+  end
+
+  def decorate_description(chapter, description)
+    output = []
+    output << "##{chapter}" if chapter.present?
+    output << description
+    output.join(' ')
   end
 end
